6e741dc4854179ec8b978f85d7ff84b83c81620f
nikita_jaiswal <nikita.jaiswal@400>
2022-07-06 20:46:19 +0530

    linkedin login success

--- backend/routes.js
+++ backend/routes.js
@@ -9,82 +9,94 @@ router.get("/", (req, res) => {
     res.status(200).send("Welcome 🙌 ");
 });
 //linked in
-router.get('/getUserCredentials',async (req, res) => {
-    const user = {};
-    const code = req.query.code;
-    const accessToken = await getAccessToken(code);
-    // const userProfile = await getUserProfile(accessToken);
-    // const userEmail = await getUserEmail(accessToken);
-    res.status(200).json({ accessToken});
-    // let resStatus = 400;
+const urlToGetLinkedInAccessToken = 'https://www.linkedin.com/oauth/v2/accessToken';
+const urlToGetUserProfile ='https://api.linkedin.com/v2/me?projection=(id,localizedFirstName,localizedLastName,profilePicture(displayImage~digitalmediaAsset:playableStreams))'
+const urlToGetUserEmail = 'https://api.linkedin.com/v2/clientAwareMemberHandles?q=members&projection=(elements*(primary,type,handle~))';
+
+
+router.get('/getUserCredentials',async(req, res) => {
+
+  const code = req.query.code;
+  const accessToken = await getAccessToken(code);
+  const userProfile = await getUserProfile(accessToken);
+  const userEmail = await getUserEmail(accessToken);
+  let resStatus = 400;
+  if(!(accessToken === null || userProfile === null || userEmail === null)) {
+    user = userBuilder(userProfile, userEmail);
+    resStatus = 200;
+  }
+  // Here, you can implement your own login logic 
+  // to authenticate new user or register him
+  res.status(resStatus).json({ userProfile });
   
-    // if(!(accessToken === null || userProfile === null || userEmail === null)) {
-    //   user = userBuilder(userProfile, userEmail);
-    //   resStatus = 200;
-    // }
-    // res.status(resStatus).json({ user });
 })
   
   
   //get access token
   async function getAccessToken(code) {
-    let accessToken = null;
-    const parameters = {
-      "grant_type": "authorization_code",
-      "code": code,
-      "redirect_uri": 'http://localhost:3000',
-      "client_id": '771uzz96w5lajg',
-      "client_secret": 'opyNwAyLi2uP3hyU',
-    };
-    const config = {
+
+    var data = qs.stringify({
+      'grant_type': 'authorization_code',
+      'redirect_uri': 'http://localhost:3000',
+      'client_id': '771uzz96w5lajg',
+      'client_secret': 'opyNwAyLi2uP3hyU',
+      'code':code
+    });
+    var config = {
+      method: 'post',
+      url: 'https://www.linkedin.com/oauth/v2/accessToken',
       headers: { 
-        'Content-Type': 'application/x-www-form-urlencoded'
-       }
+        'Content-Type': 'application/x-www-form-urlencoded', 
+      },
+      data : data
     };
-  
-    await axios.post('https://www.linkedin.com/oauth/v2/accessToken',qs.stringify(parameters), config)
+    
+    return await axios(config)
     .then(function (response) {
-      return response.data
+      return response.data.access_token
     })
     .catch(function (error) {
-      console.log('error', error);
-      return null
+      console.log(error);
+      return error
     });
   }
   
   async function getUserProfile(accessToken) {
-    let userProfile = null;
-    const config = {
-      headers: {
-        "Authorization": `Bearer ${accessToken}`
-      }
-    }
-    await axios
-      .get(urlToGetUserProfile, config)
-      .then(response => {
-        userProfile.firstName = response.data["localizedFirstName"];
-        userProfile.lastName = response.data["localizedLastName"];
-        userProfile.profileImageURL = response.data.profilePicture["displayImage~"].elements[0].identifiers[0].identifier;
-        // I mean, couldn't they have burried it any deeper?
-      })
-      .catch(error => console.log("Error grabbing user profile"))
-    return userProfile;
-  }
-  async function getUserEmail(accessToken) {
-    const email = null;
-    const config = {
-      headers: {
-        "Authorization": `Bearer ${accessToken}`
+
+    var config = {
+      method: 'get',
+      url: urlToGetUserProfile,
+      headers: { 
+        'Authorization': 'Bearer '+accessToken
       }
     };
-    await axios
-      .get(urlToGetUserEmail, config)
-      .then(response => {
-        email = response.data.elements[0]["handle~"];
-      })
-      .catch(error => console.log("Error getting user email"))
-  
-    return email;
+    
+    return await axios(config)
+    .then(function (response) {
+      return response.data
+    })
+    .catch(function (error) {
+      return error;
+    });
+  }
+
+  async function getUserEmail(accessToken) {
+    var config = {
+      method: 'get',
+      url: urlToGetUserEmail,
+      headers: { 
+        'Authorization': 'Bearer ' + accessToken
+      }
+    };
+    
+    return await axios(config)
+    .then(function (response) {
+      return response.data;
+    })
+    .catch(function (error) {
+      return error;
+    });
+    
   }
   function userBuilder(userProfile, userEmail) {
     return {
--- login/src/App.js
+++ login/src/App.js
@@ -2,9 +2,10 @@ import { useEffect, useState } from 'react';
 import linkedin from './images/in.png';
 import Google from './images/google.png';
 import Twitter from './images/twiiter.png';
+import Facebook from './images/facebook.jpg';
 import axios from 'axios';
 import './App.css'
-import { LoginSocialGoogle, LoginSocialTwitter} from 'reactjs-social-login';
+import { LoginSocialGoogle, LoginSocialTwitter, LoginSocialFacebook} from 'reactjs-social-login';
 const CLIENT_ID = '771uzz96w5lajg';
 const REDIRECT_URL = 'http://localhost:3000';
 const OAUTH_URL = 'https://www.linkedin.com/oauth/v2/authorization?response_type=code';
@@ -13,15 +14,7 @@ const STATE = '123456';
 
 function App() {
   
-  const [providet, setProvider] = useState('')
-  useEffect(()=>{
-    if (window.opener && window.opener !== window) {
-      const code = getCodeFromWindowURL(window.location.href);
-      window.opener.postMessage({'type': 'code', 'code': code}, '*')
-      window.close();
-    }
-      window.addEventListener('message', handlePostMessage);
-  }, [])
+  const [providet, setProvider] = useState('');
 
   const getCodeFromWindowURL = url => {
     const popupWindowURL = new URL(url);
@@ -31,7 +24,7 @@ function App() {
   const handlePostMessage = event => {
     if (event.data.type === 'code') {
       const { code } = event.data;
-      getUserCredentials(code);
+      getUserCredentials(event.data.code);
     }
   };
 
@@ -40,32 +33,40 @@ function App() {
       .get(`http://localhost:5000/getUserCredentials?code=${code}`)
       .then(res => {
         const user = res.data;
-        this.setState({
-          user,
-          loaded: true
-        })
-        // Do something with user
       });
   };
+
   const showPopup = () => {
     const oauthUrl = `${OAUTH_URL}&client_id=${CLIENT_ID}&scope=${SCOPE}&state=${STATE}&redirect_uri=${REDIRECT_URL}`;
+
     const width = 450,
       height = 730,
       left = window.screen.width / 2 - width / 2,
       top = window.screen.height / 2 - height / 2;
-    window.open(
-      oauthUrl,
-      'Linkedin',
-      'menubar=no,location=no,resizable=no,scrollbars=no,status=no, width=' +
-      width +
-      ', height=' +
-      height +
-      ', top=' +
-      top +
-      ', left=' +
-      left
-    );
+      window.open(
+        oauthUrl,
+        'Linkedin',
+        'menubar=no,location=no,resizable=no,scrollbars=no,status=no, width=' +
+        width +
+        ', height=' +
+        height +
+        ', top=' +
+        top +
+        ', left=' +
+        left
+      );
   };
+
+  useEffect(() => {
+    console.log('window.opener',  window.opener !== window);
+    if (window.opener && window.opener !== window) {
+      const code = getCodeFromWindowURL(window.location.href);
+      console.log('code', code);
+      window.opener.postMessage({'type': 'code', 'code': code}, 'http://localhost:3000')
+      window.close();
+    }
+    window.addEventListener('message', handlePostMessage);
+}, [])
 
   return (
     <div className="App">
@@ -94,6 +95,16 @@ function App() {
           >
             <img src={Twitter} alt="twiiter"/>
         </LoginSocialTwitter>
+      </div>
+      <div className='facebook'>
+        <LoginSocialFacebook 
+          appId='1432856887142206'
+          onResolve={( provider ) => {
+            console.log('provider', provider);
+            setProvider(provider)
+          }}>
+          <img src={Facebook} alt="facebook"/>
+        </LoginSocialFacebook>
       </div>
     </div>
   );
